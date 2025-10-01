c9f13bf1a069b4e05e98dfcb73e1343ddf8cf972
sunflowermm <1814632762@qq.com>
2025-10-01 10:31:18 +0800

    Update: 2025-10-01 10:31 (2 modified)

--- plugins/events/online.js
+++ plugins/events/online.js
@@ -12,6 +12,34 @@ export default class onlineEvent extends EventListener {
   }
 
   async execute() {
-     Bot.makeLog("info", `尽情享受吧QaQ`, 'event')
+    Bot.makeLog("info", `尽情享受吧QaQ`, 'event')
+    const currentUin = e?.self_id || Bot.uin[0]
+    if (!currentUin) {
+      logger.debug('无法获取机器人QQ号，跳过重启消息发送')
+      return
+    }
+    
+    let restart = await redis.get(`${this.key}:${currentUin}`)
+    if (!restart) {
+      logger.debug('没有检测到重启信息，机器人正常启动')
+      return
+    }
+    
+    try {
+      restart = JSON.parse(restart)
+      let time = restart.time || new Date().getTime()
+      time = (new Date().getTime() - time) / 1000
+      
+      let msg = `重启成功，耗时${time.toFixed(2)}秒`
+      
+      if (restart.isGroup) {
+        await Bot[currentUin].pickGroup(restart.id).sendMsg(msg)
+      } else {
+        await Bot[currentUin].pickUser(restart.id).sendMsg(msg)
+      }
+      await redis.del(`${this.key}:${currentUin}`)
+    } catch (error) {
+      logger.error(`发送重启消息失败：${error}`)
+    }
   }
 }
